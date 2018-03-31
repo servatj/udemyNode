a058b9db9c35e5225c077c5727d87a8c4aa3a82d
Josep Servat <josep.servat@guidesmiths.com>
2018-03-31 20:08:43 +0200

    initial commit Ethereum Project on node

---
+++ compile.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+solc.compile(source, 1); // the second param is for the number of contract sources we want to compile 
+
+console.log(solc.compile(source,1));
