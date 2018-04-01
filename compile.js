f5f9cc48b7d4293a4eade0811b2c2b4988b26be1
Josep Servat <josep.servat@guidesmiths.com>
2018-04-01 02:36:31 +0200

    first running solitiy test before sleep

--- compile.js
+++ compile.js
@@ -2,10 +2,12 @@ const path = require('path');
 const fs = require('fs');
 
 const solc = require('solc');
-
+        
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 solc.compile(source, 1); // the second param is for the number of contract sources we want to compile 
 
-console.log(solc.compile(source,1));
+//console.log(solc.compile(source,1));
+
+module.exports = solc.compile(source,1).contracts[':Inbox']
